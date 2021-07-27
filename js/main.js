56f2a54341c8af91b4fa907baf6eae0522ed81d7
ngf93 <nabiullinaguzel93@gmail.com>
2021-07-27 18:17:47 +0300

    save 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-/* current year */
-// let now = new Date();
-// let cur_year = now.getFullYear();
-// document.querySelector('#year').innerHTML = cur_year;
-
-
-const swiper = new Swiper('.swiper-4', {
-  slidesPerView: 4,
-  spaceBetween: 16,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+/* фиксированная шапка */
+window.addEventListener('scroll', function() {
+  let h_header = document.querySelector('header').offsetHeight;
+  let st = window.pageYOffset;
+  if (st > h_header) {
+    document.querySelector('header').classList.add("h-sticky");
+  } else {
+    document.querySelector('header').classList.remove("h-sticky");
+  }
 });
+
+$(function() {
+  $(".toggle-cart").mouseenter(function() {
+    $('.cart-preview').show();
+  });
+  $(".toggle-cart").mouseleave(function() {
+    $('.cart-preview').hide();
+  });
+});
+
+function toggleCart(parent){
+  // parent.querySelector('.cart-preview').style.display = 'block';
+  parent.querySelector('.cart-preview').style.display = (parent.querySelector('.cart-preview').style.display == 'block') ? 'none' : 'block'
+}
---
+++ js/sliders.js
@@ -0,0 +1,21 @@
+const swiper1 = new Swiper('.swiper-4', {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+  
+const swiper2 = new Swiper('.swiper-3', {
+slidesPerView: 3,
+spaceBetween: 16,
+pagination: {
+    el: '.swiper-pagination',
+},
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+});
